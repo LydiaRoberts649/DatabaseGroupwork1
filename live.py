b1824ce7909c54d95de2f93915dff3c217497382
Lydia <lydiamuchemi@gmail.com>
2025-03-23 18:22:13 +0300

    initial commit

---
+++ live.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number"))
+
+print("The sum of the two numbers is:",num1+num2)
+print("The difference between the two numbers is:",num1-num2)
+print("The division of the two numbers is:" ,num1/num2)
+print("The multiplication  of the two numbers is:",num1*num2)
